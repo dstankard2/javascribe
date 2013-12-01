cee1b617cffefdf05154362a6fa9d3ac2ec0930f
dstankard2 <dstankard2@gmail.com>
2013-12-01 18:47:40 -0500

    Documentation of Injectable.

--- language-support/src/main/java/net/sf/javascribe/langsupport/java/Injectable.java
+++ language-support/src/main/java/net/sf/javascribe/langsupport/java/Injectable.java
@@ -3,8 +3,20 @@ package net.sf.javascribe.langsupport.java;
 import net.sf.javascribe.api.CodeExecutionContext;
 import net.sf.javascribe.api.JavascribeException;
 
+/**
+ * Represents an object that can be injected as a dependency into another object
+ * @author DCS
+ */
 public interface Injectable extends JavaVariableType {
 
+	/**
+	 * Returns the code required to declare and instantiate the type in a ready-to-inject state.
+	 * @param instanceName
+	 * @param execCtx
+	 * @return
+	 * @throws JavascribeException
+	 */
 	public JavaCode getInstance(String instanceName,CodeExecutionContext execCtx) throws JavascribeException;
 
 }
+
