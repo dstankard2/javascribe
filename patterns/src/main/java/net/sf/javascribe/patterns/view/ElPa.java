567748f77799fa4ad18240793c8a5c83f964be4f
dstankard2 <dstankard2@gmail.com>
2015-11-19 17:57:45 -0500

    Required for HTML templating.

---
+++ patterns/src/main/java/net/sf/javascribe/patterns/view/ElPa.java
@@ -0,0 +1,328 @@
+package net.sf.javascribe.patterns.view;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import net.sf.javascribe.api.CodeExecutionContext;
+import net.sf.javascribe.api.JavascribeException;
+import net.sf.javascribe.api.ProcessorContext;
+import net.sf.javascribe.api.VariableType;
+import net.sf.javascribe.langsupport.javascript.JavascriptBaseObjectType;
+import net.sf.javascribe.langsupport.javascript.JavascriptFunctionType;
+import net.sf.javascribe.patterns.view.impl.JaEval2;
+import net.sf.javascribe.patterns.view.impl.JaEvalResult;
+
+import org.jsoup.nodes.Attribute;
+import org.jsoup.nodes.Element;
+
+public class ElPa implements DirectiveContext {
+	Map<String,ElementDirective> elementDirectives = null;
+	List<AttributeDirective> attributeDirectives = null;
+
+	Map<String,String> attributes = new HashMap<String,String>();
+	Map<String,String> domAttributes = new HashMap<String,String>();
+	Map<String,String> templateAttributes = new HashMap<String,String>();
+
+	ProcessorContext ctx = null;
+	Element elt = null;
+	String containerVar = null;
+	StringBuilder code = new StringBuilder();
+	String templateObj = null;
+	JavascriptFunctionType function = null;
+	List<String> previousEltVars = null;
+	TempPars caller = null;
+
+	String eltVar = null;
+
+	boolean elementDirectiveCalled = false;
+	boolean isTemplateCall = false;
+	boolean elementCreated = false;
+	boolean domPropertiesAdded = false;
+	List<CodeExecutionContext> contexts = new ArrayList<CodeExecutionContext>();
+
+	public String getElementCode() {
+		return code.toString();
+	}
+	public String getEltVar() {
+		return eltVar;
+	}
+	
+	public ElPa(Element elt,ProcessorContext ctx,String containerVar,String templateObj,JavascriptFunctionType fn,List<String> previousElementVars, TempPars caller) {
+		this.elt = elt;
+		this.ctx = ctx;
+		this.containerVar =containerVar;
+		this.templateObj = templateObj;
+		this.function = fn;
+		this.previousEltVars = previousElementVars;
+		this.caller = caller;
+	}
+
+	public void parseElement(CodeExecutionContext execCtx) throws JavascribeException {
+		attributeDirectives = DirectiveUtils.getAttributeDirectives(ctx);
+		elementDirectives = DirectiveUtils.getElementDirectives(ctx);
+
+		isTemplateCall = elt.nodeName().indexOf('.')>0;
+		
+		for(Attribute att : elt.attributes().asList()) {
+			String key = att.getKey();
+			String val = att.getValue();
+			attributes.put(key, val);
+			if (key.startsWith("js-")) {
+				templateAttributes.put(key, val);
+			} else {
+				domAttributes.put(key, val);
+			}
+		}
+		
+		eltVar = DirectiveUtils.newVarName("_e", "DOMElement", execCtx);
+		
+		code.append("var "+eltVar+";\n");
+		
+		this.continueParsing(execCtx);
+	}
+
+	private void continueParsing(CodeExecutionContext execCtx) throws JavascribeException {
+		contexts.add(execCtx);
+		if (attributeDirectives.size()>0) {
+			AttributeDirective next = attributeDirectives.get(0);
+			attributeDirectives.remove(0);
+			if ((!elementCreated) && (next.getPriority()>=3) 
+					&& (!isTemplateCall) && (elementDirectives.get(elt.nodeName())==null)) {
+				elementCreated = true;
+				code.append(eltVar+" = "+DirectiveUtils.DOCUMENT_REF+
+						".createElement('"+elt.nodeName()+"');\n");
+				code.append(eltVar+"._elt = '"+eltVar+"';\n");
+				if (templateAttributes.get("js-event")==null) {
+					code.append(containerVar+".appendChild("+eltVar+");\n");
+				}
+			}
+			String att = next.getAttributeName();
+			if (templateAttributes.get(att)!=null) {
+				next.generateCode(this);
+			} else {
+				continueParsing(execCtx);
+			}
+		} else {
+			// Element directive or template call or element
+			if (isTemplateCall) {
+				code.append(processTemplateCall(elt.nodeName(),execCtx,this));
+			} else if (elementDirectives.get(elt.nodeName())!=null) {
+				ElementDirective d = elementDirectives.get(elt.nodeName());
+				d.generateCode(this);
+			} else {
+				addDomProperties(execCtx);
+				// Process children
+				if ((elt.childNodes()!=null) && (elt.childNodes().size()>0)) {
+					String append = caller.processChildNodeList(elt.childNodes(), eltVar, execCtx);
+					code.append(append);
+				}
+			}
+		}
+		contexts.remove(contexts.size()-1);
+	}
+	
+	private void addDomProperties(CodeExecutionContext execCtx) throws JavascribeException {
+		for(String s : domAttributes.keySet()) {
+		//for(String s : rctx.getDomAttributes().keySet()) {
+			if (s.equals("class")) continue;
+			String val = domAttributes.get(s);
+			//String val = rctx.getDomAttributes().get(s).trim();
+			if (s.equals("style")) code.append(addStyle(eltVar,val));
+			else {
+				String ref = DirectiveUtils.parsePartialExpression(val, execCtx);
+				if (ref==null) {
+					throw new JavascribeException("Couldn't evaluate attribute '"+s+"' with value '"+val+"'");
+				}
+				if (s.indexOf('-')>=0) {
+					code.append(eltVar+".setAttribute('"+s+"',"+ref+");\n");
+				} else {
+					code.append(eltVar+"."+s+" = "+ref+";\n");
+				}
+			}
+		}
+		if ((elt.id()!=null) && (elt.id().trim().length()>0)) {
+			code.append(eltVar+".id = '"+elt.id()+"';\n");
+		}
+		if ((elt.className()!=null) && (elt.className().trim().length()>0)) {
+			String t = elt.className().trim();
+			String val = DirectiveUtils.parsePartialExpression(t, execCtx);
+			code.append(eltVar+".className = "+val+";\n");
+		}
+	}
+
+	protected String processTemplateCall(String elementName,CodeExecutionContext execCtx,DirectiveContext dctx) throws JavascribeException {
+		// Take the element name and remove '-', insert capital letter
+		int i = elementName.indexOf('-');
+		String temp = elementName;
+		while(i>0) {
+			if (i==elementName.length()-1) {
+				throw new JavascribeException("A HTML element node name cannot end with '.' ("+temp+")");
+			} else if (i==0) {
+				throw new JavascribeException("A HTML element node name cannot start with '.' ("+temp+")");
+			}
+			if (!Character.isLetter(elementName.charAt(i+1))) {
+				throw new JavascribeException("HTML template parser found an invalid node name '"+temp+"'");
+			}
+			elementName = elementName.substring(0,i)+Character.toUpperCase(elementName.charAt(i+1))+elementName.substring(i+2);
+			i = elementName.indexOf('-');
+		}
+		i = elementName.lastIndexOf('.');
+		if ((i==elementName.length()-1) || (i==0)) {
+			throw new JavascribeException("Found an invalid template reference '"+elementName+"'");
+		}
+		String objRef = elementName.substring(0,i);
+		String ruleName = elementName.substring(i+1);
+		VariableType type = execCtx.evaluateVariableTypeForExpression(objRef);
+		if ((type==null) || (!(type instanceof JavascriptBaseObjectType))) {
+			throw new JavascribeException("Couldn't find template '"+elementName+"'");
+		}
+		JavascriptBaseObjectType srv = (JavascriptBaseObjectType)type;
+		List<JavascriptFunctionType> fns = srv.getOperations(ruleName);
+		if ((fns==null) || (fns.size()==0)) {
+			throw new JavascribeException("Couldn't find template '"+elementName+"'");
+		} else if (fns.size()>1) {
+			throw new JavascribeException("Found multiple templates called '"+elementName+"' - couldn't determine which to invoke");
+		}
+		JavascriptFunctionType fn = fns.get(0);
+		Map<String,String> atts = dctx.getDomAttributes();
+		HashMap<String,String> params = new HashMap<String,String>();
+		Iterator<String> names = atts.keySet().iterator();
+		while(names.hasNext()) {
+			String htmlAttr = names.next();
+			String n = findLowerCamelFromHtml(htmlAttr);
+			String val = atts.get(htmlAttr);
+			JaEval2 eval = new JaEval2(val,execCtx);
+			DirectiveUtils.populateImpliedVariables(eval);
+			JaEvalResult result = eval.parseExpression();
+			if (result.getErrorMessage()!=null) {
+				throw new JavascribeException("Couldn't build template call - "+result.getErrorMessage());
+			}
+			String p = "(function() {try {return "+result.getResult().toString()+";}catch(err) { return undefined; } })()";
+			params.put(n, p);
+		}
+		return fn.invoke(dctx.getElementVarName(), objRef, params, execCtx);
+	}
+	protected static String findLowerCamelFromHtml(String n) {
+		int i = n.indexOf('-');
+		while(i>=0) {
+			if (i==n.length()-1) n = n.substring(0, i-1);
+			else if (i==n.length()-2) {
+				n = n.substring(0, i) + Character.toUpperCase(n.charAt(i+1));
+			}
+			else if (i==0) {
+				n = "" + Character.toUpperCase(n.charAt(1)) + n.substring(2);
+			} else {
+				n = n.substring(0, i) + Character.toUpperCase(n.charAt(i+1)) + n.substring(i+2);
+			}
+			i = n.indexOf('-');
+		}
+		return n;
+	}
+	
+	protected String addStyle(String elt,String value) {
+		StringBuilder b = new StringBuilder();
+		StringTokenizer tok = new StringTokenizer(value,";");
+		
+		while(tok.hasMoreTokens()) {
+			String token = tok.nextToken();
+			if ((token.trim().length()==0)) continue;
+			int i = token.indexOf(':');
+			if (i>0) {
+				String prop = token.substring(0, i).trim();
+				while(prop.indexOf('-')>=0) {
+					int x = prop.indexOf('-');
+					prop = prop.substring(0, x) + Character.toUpperCase(prop.charAt(x+1))
+							+ prop.substring(x+2);
+				}
+				String val = token.substring(i+1).trim();
+				b.append(elt+".style."+prop+" = '"+val+"';\n");
+			}
+		}
+		
+		return b.toString();
+	}
+
+	protected void invokeDirective(Directive r,CodeExecutionContext execCtx,DirectiveContextImpl rctx) throws JavascribeException {
+		CodeExecutionContext origCtx = rctx.getExecCtx();
+		rctx.setExecCtx(execCtx);
+		r.generateCode(rctx);
+		rctx.setExecCtx(origCtx);
+	}
+	
+	public String newVarName(String baseName,String type,CodeExecutionContext execCtx) {
+		for(int i=0;i<10000;i++) {
+			String s = baseName+i;
+			if (execCtx.getVariableType(s)==null) {
+				execCtx.addVariable(s, type);
+				return s;
+			}
+		}
+		return null;
+	}
+
+	protected boolean isRestriction(Restrictions type,Restrictions[] restrictions) {
+		for(Restrictions r : restrictions) {
+			if (r==type) return true;
+		}
+		return false;
+	}
+	@Override
+	public ProcessorContext getProcessorContext() {
+		return ctx;
+	}
+	@Override
+	public CodeExecutionContext getExecCtx() {
+		if (contexts.size()==0) return null;
+		return contexts.get(contexts.size()-1);
+	}
+	@Override
+	public String getElementName() {
+		return elt.nodeName();
+	}
+	@Override
+	public Map<String, String> getDomAttributes() {
+		return this.domAttributes;
+	}
+	@Override
+	public Map<String, String> getTemplateAttributes() {
+		return this.templateAttributes;
+	}
+	@Override
+	public String getElementVarName() {
+		return this.eltVar;
+	}
+	@Override
+	public String getContainerVarName() {
+		return this.containerVar;
+	}
+	@Override
+	public void continueRenderElement(CodeExecutionContext execCtx) throws JavascribeException {
+		this.continueParsing(execCtx);
+	}
+	@Override
+	public String getInnerHtml() {
+		return elt.html();
+	}
+	@Override
+	public String getTemplateObj() {
+		return this.templateObj;
+	}
+	@Override
+	public JavascriptFunctionType getFunction() {
+		return this.function;
+	}
+	@Override
+	public List<String> getPreviousEltVars() {
+		return previousEltVars;
+	}
+	
+	public StringBuilder getCode() {
+		return this.code;
+	}
+
+}
+
---
+++ patterns/src/main/java/net/sf/javascribe/patterns/view/TempPars.java
@@ -0,0 +1,104 @@
+package net.sf.javascribe.patterns.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.javascribe.api.CodeExecutionContext;
+import net.sf.javascribe.api.JavascribeException;
+import net.sf.javascribe.api.ProcessorContext;
+import net.sf.javascribe.langsupport.javascript.JavascriptFunctionType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.parser.Parser;
+
+public class TempPars {
+	String template;
+	ProcessorContext ctx;
+	String obj;
+	JavascriptFunctionType fn;
+	
+	public TempPars(String template,ProcessorContext ctx,String obj,JavascriptFunctionType fn) {
+		this.template = template;
+		this.ctx = ctx;
+		this.obj = obj;
+		this.fn = fn;
+	}
+	
+	public String generateJavascriptCode(CodeExecutionContext execCtx) throws JavascribeException {
+		StringBuilder b = new StringBuilder();
+		Document doc = Jsoup.parse(template.trim(), "//", Parser.xmlParser());
+
+		List<Node> nodeList = doc.childNodes();
+		if (nodeList.size()>1) {
+			throw new JavascribeException("TemplateParser doesn't support a template with multiple HTML elements at the root.  Perhaps use a container 'div' element");
+		}
+
+		b.append("var "+DirectiveUtils.DOCUMENT_REF+" = document;\n");
+		String retVar = DirectiveUtils.TEMPLATE_ROOT_ELEMENT_REF;
+		b.append("var "+retVar+" = document.createElement('div');\n");
+		execCtx.addVariable(retVar, "DOMElement");
+		b.append(processChildNodeList(nodeList,retVar,execCtx));
+
+		b.append("return "+retVar+".childNodes[0];\n");
+		
+		return b.toString();
+	}
+
+	public String processChildNodeList(List<Node> nodeList,String containerVar,CodeExecutionContext execCtx) throws JavascribeException {
+		StringBuilder b = new StringBuilder();
+		List<String> children = new ArrayList<String>();
+		
+		b.append("(function() {\n");
+
+		CodeExecutionContext newCtx = new CodeExecutionContext(execCtx,ctx.getTypes());
+		for(Node node : nodeList) {
+			if (node.toString().trim().length()>0) {
+				b.append(parseNode(containerVar,node,newCtx,children));
+			}
+		}
+		
+		b.append("})();");
+		
+		return b.toString();
+	}
+
+	public String parseNode(String containerVar,Node node,CodeExecutionContext execCtx,List<String> previousEltVars) throws JavascribeException {
+		String ret = null;
+		
+		if (node instanceof Element) {
+			Element elt = (Element)node;
+			ElPa parser = new ElPa(elt,ctx,containerVar,obj,fn,previousEltVars,this);
+			parser.parseElement(execCtx);
+			ret = parser.getElementCode();
+		} else if (node instanceof TextNode) {
+			ret = processTextNode((TextNode)node,containerVar,execCtx);
+		}
+		
+		return ret;
+	}
+
+	protected static String processTextNode(TextNode n,String containerVar,CodeExecutionContext execCtx) throws JavascribeException {
+		String text = n.text();
+		String docRef = DirectiveUtils.DOCUMENT_REF;
+		String finalRef = DirectiveUtils.parsePartialExpression(text, execCtx);
+		String var = DirectiveUtils.newVarName("_t","DOMElement",execCtx);
+		StringBuilder code = new StringBuilder();
+
+		if (text.trim().length()==0) return null;
+
+		code.append("try {\n");
+		code.append("var "+var+" = "+docRef+".createTextNode(");
+		code.append(finalRef);
+		code.append(");\n");
+
+		code.append(containerVar+".appendChild("+var+");\n");
+		code.append("}catch(_err){}\n");
+		return code.toString();
+	}
+	
+}
+
